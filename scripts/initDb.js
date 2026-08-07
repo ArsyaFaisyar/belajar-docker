77aad09acbb397a7e71c1e3650eee9042b3b7bdb
Nath2006 <ananthapindika739@gmail.com>
2026-08-07 10:56:20 +0700

    fix : fix error jest testing

--- scripts/initDb.js
+++ scripts/initDb.js
@@ -1,26 +1,22 @@
-/**
- * ini untuk testing DB pas update fitur
+/***
+ * Ini untuk testing db pas update fitur
  */
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
-const fs = require("node:fs/promises")
-const path = require("node:path")
-
-const pool = require("../src/config/database.js")
-const { error } = require("node:console")
+const pool = require('../src/config/database');
 
 async function initDatabase() {
-    const sqlPath = path.join(__dirname, "", "db", "init.sql")
-    const sql = await fs.readFile(sqlPath, "utf-8")
-    
-    await pool.query(sql)
-    console.log("database berhasil di inisiasi")
-    
+  const sqlPath = path.join(__dirname, "..", "db", "init.sql");
+  const sql = await fs.readFile(sqlPath, 'utf8');
 
-    initDatabase().catch((error) => {
-        console.log("gagal inisiasi database", error)
-        process.exitCode = 1
-    }).finally(async () => {
-        await pool.end()
-    })
+  await pool.query(sql);
+  console.log("Database Berhasil diinisialisasikan!");
 }
 
+initDatabase().catch((error) => {
+  console.log(`Gagal menginisalisasi database: `, error);
+  process.exitCode = 1;
+}).finally(async () => {
+  await pool.end();
+});
